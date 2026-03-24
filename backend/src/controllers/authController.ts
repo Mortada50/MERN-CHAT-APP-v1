00f46dc482acfbe4801ff718ac7e83532b52d3ea
Mortada50 <mortadaabdullahaltaj@gmail.com>
2026-03-25 02:36:24 +0300

    feat: integrate Clerk authentication and set up user routes

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,46 @@
+import type { AuthRequest } from "../middleware/auth";
+import type { Response, Request, NextFunction } from "express";
+import { User } from "../models/User.ts";
+import { clerkClient, getAuth } from "@clerk/express";
+
+export async function getMe(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+        const userId = req.userId;
+        
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({message: "User not found"});
+        return res.status(200).json(user)
+    } catch (error) {
+        res.status(500);
+        next();
+    }
+}
+
+export async function authCallback(req: Request, res: Response, next: NextFunction) {
+
+    try {
+        const {userId: clerkId} = getAuth(req);
+        if(!clerkId) return res.status(401).json({message: "Unauthorized"})
+        
+        let user = await User.findOne({clerkId});
+        if(!user){
+            // get user info from clerk and save to db
+            const clerkUser = await clerkClient.users.getUser(clerkId);
+
+            user = await User.create({
+                clerkId,
+                name: clerkUser.firstName 
+                      ? `${clerkUser.firstName} ${clerkUser.lastName || ""}`.trim()
+                      : clerkUser.emailAddresses[0]?.emailAddress.split("@")[0],
+                email: clerkUser.emailAddresses[0]?.emailAddress,
+                avatar: clerkUser.imageUrl
+                    
+            });
+        }
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(500);
+        next(error);
+    }
+
+}
