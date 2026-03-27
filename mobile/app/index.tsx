8d9a38c75eec0df57df0411f65aff7ebc4fcc2bf
Mortada50 <mortadaabdullahaltaj@gmail.com>
2026-03-27 17:22:13 +0300

    feat: add custom hook for social authentication using Clerk SSO

--- mobile/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View>
-      <Text className="text-red-500">hello world</Text>
-    </View>
-  );
-}
