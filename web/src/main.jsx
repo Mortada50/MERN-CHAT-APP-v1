571ac2a513ee2fe3bafe4a83f0b26c86f5e58abc
Mortada50 <mortadaabdullahaltaj@gmail.com>
2026-04-01 00:37:53 +0300

    fix publishableKey

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishablekey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <App />
